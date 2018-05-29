f81541ac9bd7b004ba222e4b0e8f4666c8f20777
Joe Bedard <mr.joe.bedard@gmail.com>
2018-05-29 18:20:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Visage Face Tracking
+==========
+This is the Visage Face Tracking Ubitrack submodule.
+
+Description
+----------
+Ubitrack uses Visage Face Tracking for head pose estimation.
+
+Usage
+-----
+In order to use it, you have to clone the buildenvironment, change to the ubitrack directory and add this module by executing:
+
+    git submodule add https://github.com/Ubitrack/directshow.git modules/directshow
+
+
+Dependencies
+----------
+The Visage SDK: 
+http://visagetechnologies.com/
---
+++ src/VisageFaceTracking.cpp
@@ -0,0 +1,164 @@
+/*
+ * Ubitrack - Library for Ubiquitous Tracking
+ * Copyright 2006, Technische Universitaet Muenchen, and individual
+ 3* contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/**
+ * @ingroup vision_components
+ * @file
+ * 
+ *
+ * @author 
+ */
+
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <iomanip>
+#include <strstream>
+#include <log4cpp/Category.hh>
+
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/scoped_array.hpp>
+
+#include <utDataflow/PushSupplier.h>
+#include <utDataflow/PullSupplier.h>
+#include <utDataflow/Component.h>
+#include <utDataflow/ComponentFactory.h>
+#include <utMeasurement/Measurement.h>
+#include <utMeasurement/TimestampSync.h>
+#include <utUtil/OS.h>
+#include <utUtil/TracingProvider.h>
+#include <opencv/cv.h>
+#include <utVision/Image.h>
+#include <utVision/Undistortion.h>
+
+#include <visageVision.h>
+
+// get a logger
+static log4cpp::Category& logger( log4cpp::Category::getInstance( "Ubitrack.Vision.VisageFaceTracking" ) );
+
+using namespace Ubitrack;
+using namespace Ubitrack::Vision;
+
+namespace Ubitrack { namespace Drivers {
+
+/**
+ * @ingroup vision_components
+ *
+ * @par Input Ports
+ * None.
+ *
+ * @par Output Ports
+ * \c Output push port of type Ubitrack::Measurement::ImageMeasurement.
+ *
+ * @par Configuration
+ * The configuration tag contains a \c <dsvl_input> configuration.
+ * For details, see the DirectShow documentation...
+ *
+ */
+class VisageFaceTracking
+	: public Dataflow::Component
+{
+public:
+
+	/** constructor */
+	VisageFaceTracking( const std::string& sName, boost::shared_ptr< Graph::UTQLSubgraph >  );
+
+	/** destructor, waits until thread stops */
+	~VisageFaceTracking();
+
+	/** starts the camera */
+	void start();
+
+	
+	/** stops the camera */
+	void stop();
+
+protected:
+	
+
+	// the ports
+	Dataflow::PushConsumer< Measurement::ImageMeasurement > m_inPort;
+
+	Dataflow::PushSupplier< Measurement::Pose > m_outPort;
+	
+	void newImage(Measurement::ImageMeasurement image);
+
+private:
+	char configFile[100];
+	int * track_stat;
+	VisageTracker * m_Tracker = 0;
+};
+
+
+VisageFaceTracking::VisageFaceTracking( const std::string& sName, boost::shared_ptr< Graph::UTQLSubgraph > subgraph )
+	: Dataflow::Component( sName )	
+	, m_outPort( "Output", *this )
+	, m_inPort( "ImageInput", *this, boost::bind(&VisageFaceTracking::newImage, this, _1))
+{
+	m_Tracker = new VisageTracker(configFile);
+}
+
+void VisageFaceTracking::newImage(Measurement::ImageMeasurement image) {
+	//image->channels
+	
+	//track_stat = tracker->track(framePtr->width, framePtr->height, framePtr->imageData, &faceData, format, framePtr->origin);
+
+	Math::Quaternion headRot = Math::Quaternion(0, 0, 0, 1);
+	Math::Vector3d headPos = Math::Vector3d(0, 0, 0);
+	Math::Pose headPose = Math::Pose(headRot, headPos);
+	Measurement::Pose meaHeadPose = Measurement::Pose(image.time(), headPose);
+	m_outPort.send(meaHeadPose);
+}
+
+
+VisageFaceTracking::~VisageFaceTracking()
+{
+	track_stat = tracker->track(0, 0, 0, 0);
+	delete m_Tracker;
+}
+
+
+void VisageFaceTracking::start()
+{
+	
+	Component::start();
+}
+
+
+void VisageFaceTracking::stop()
+{
+	
+	Component::stop();
+}
+
+
+
+} } // namespace Ubitrack::Driver
+
+UBITRACK_REGISTER_COMPONENT( Dataflow::ComponentFactory* const cf ) {
+	cf->registerComponent< Ubitrack::Drivers::VisageFaceTracking > ( "VisageFaceTracking" );
+}
+
